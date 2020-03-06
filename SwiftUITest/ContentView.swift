d59051ef8298d696175d26b3f717bc5d2e53f252
Takamasa Fukase <takama224@yahoo.co.jp>
2020-03-06 21:21:52 +0900

    Initial Commit

---
+++ SwiftUITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by 深瀬貴将 on 2020/03/06.
+//  Copyright © 2020 fukase. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
